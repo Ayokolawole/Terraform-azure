49ade3b9cd92de9017607ead8be36ea76f39543a
Ayo <78484126+Ayokolawole@users.noreply.github.com>
2024-05-12 20:17:50 +0100

    First working terraform :)

---
+++ AKV/main.tf
@@ -0,0 +1,33 @@
+resource "azurerm_resource_group" "example" {
+  name     = "vault-testing"
+  location = "UK South"
+}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "aktestingvaultmay"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  enabled_for_disk_encryption = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name                    = "standard"  
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
